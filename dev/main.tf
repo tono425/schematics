c4420347ed368eec5eb7699b1461944221603790
Tono <tono1@jp.ibm.com>
2020-06-05 11:13:59 +0900

    add public network interface

--- dev/main.tf
+++ dev/main.tf
@@ -7,7 +7,7 @@ resource "ibm_compute_vm_instance" "tono_terraform" {
   datacenter           = "dal09"
   network_speed        = 100
   hourly_billing       = true
-  private_network_only = true
+  private_network_only = false
   cores                = 1
   memory               = 1024
   disks                = [25]
