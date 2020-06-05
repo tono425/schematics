6e01b3098c2e6f270fb5edb8a64bb3077452d238
Tono <tono1@jp.ibm.com>
2020-06-05 11:29:41 +0900

    increase core to 2

--- dev/main.tf
+++ dev/main.tf
@@ -8,7 +8,7 @@ resource "ibm_compute_vm_instance" "tono_terraform" {
   network_speed        = 100
   hourly_billing       = true
   private_network_only = false
-  cores                = 1
+  cores                = 2
   memory               = 1024
   disks                = [25]
   local_disk           = false
